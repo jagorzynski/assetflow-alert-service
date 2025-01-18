f81be84ce395e2673d3e3187117ecdf23c4958ee
Gorzynski Jakub <gorzynski.jakub@protonmail.com>
2025-01-18 16:51:07 +0100

    Fix spring default test

--- src/test/java/com/sothrose/assetflow_alert_service/AssetflowAlertServiceApplicationTests.java
+++ src/test/java/com/sothrose/assetflow_alert_service/AssetflowAlertServiceApplicationTests.java
@@ -2,7 +2,9 @@ package com.sothrose.assetflow_alert_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class AssetflowAlertServiceApplicationTests {
 
